4c40bf242e95035761f2f1e268c2cdab91630fed
Mateusz Samsel <m.samsel@cksource.com>
2019-05-20 11:52:08 +0200

    Add unit test for toolbar's label.

--- tests/decouplededitorui.js
+++ tests/decouplededitorui.js
@@ -52,6 +52,14 @@ describe( 'DecoupledEditorUI', () => {
 			expect( view.isRendered ).to.be.true;
 		} );
 
+		it( 'renders accessibility label for toolbar', () => {
+			const labelEl = view.toolbar.element.lastChild;
+
+			expect( labelEl.tagName ).to.equal( 'SPAN' );
+			expect( labelEl.classList.contains( 'ck-toolbar__label' ) ).to.be.true;
+			expect( labelEl.innerText ).to.equal( 'Editor\'s toolbar' );
+		} );
+
 		describe( 'editable', () => {
 			it( 'registers view.editable#element in editor focus tracker', () => {
 				ui.focusTracker.isFocused = false;
